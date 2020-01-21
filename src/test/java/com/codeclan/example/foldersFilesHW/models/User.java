04de52844780f096baf28705e1925bbb69b11c12
Richard <fahitch+github@gmail.com>
2020-01-21 15:33:10 +0000

    set up Ship Model

--- src/test/java/com/codeclan/example/foldersFilesHW/models/User.java
+++ src/test/java/com/codeclan/example/foldersFilesHW/models/User.java
@@ -1,10 +1,55 @@
 package com.codeclan.example.foldersFilesHW.models;
 
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String userName;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
     private List<Folder> folderList;
 
+    public User(String userName) {
+        this.userName = userName;
+        this.folderList = new ArrayList<Folder>();
+    }
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Folder> getFolderList() {
+        return new ArrayList<>(folderList);
+    }
+
+    public void setFolderList(List<Folder> folderList) {
+        this.folderList = folderList;
+    }
 }
+
