3d1c725895b06dbe7d3b9987dec95fb9d673c714
Richard <fahitch+github@gmail.com>
2020-01-21 15:30:43 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/foldersFilesHW/FoldersFilesHwApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.foldersFilesHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldersFilesHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldersFilesHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/foldersFilesHW/models/User.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.foldersFilesHW.models;
+
+import java.util.List;
+
+public class User {
+
+    private String userName;
+    private List<Folder> folderList;
+
+}
